eea2310b60b64453a32c42e2370bf6dd7d63b478
Kvnny85 <Kalonsov3@gmail.com>
2025-07-13 09:54:41 -0700

    Mobile optimizations !

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,7 +5,7 @@ import Footer from '@/components/Footer';
 import LoadingScreen from '@/components/LoadingScreen';
 import LenisProvider from '@/components/LenisProvider';
 import AOSWrapper from '@/components/AOSWrapper';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export const metadata = {
   title: 'Green Sparrow Tattoo',
   description: 'Experience tattoo artistry and visual storytelling',
