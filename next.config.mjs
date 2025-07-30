62f650c318930567dbbc68460cfdc5ab184a96f5
Kvnny85 <Kalonsov3@gmail.com>
2025-07-29 21:03:02 -0700

    fixes!

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  source: '/(.*)',
+  headers: [
+    {
+      key: 'Content-Security-Policy',
+      value: "default-src 'self'; img-src 'self' https://images.unsplash.com; script-src 'self' 'unsafe-inline' https://maps.googleapis.com; frame-ancestors 'none'"
+    },
+    {key: 'Cross-Origin-Opener-Policy', value: 'same-origin'},
+    {key: 'X-Frame-Options', value: 'DENY'}
+  ]
+};
 
 export default nextConfig;
